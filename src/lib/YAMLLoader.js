a3a305555009b928ac4bf8a41385e0e9b1bef049
David Vandensteen <dvandensteen@gmail.com>
2023-04-16 11:34:40 +0200

    Add : YAMLLoader

---
+++ src/lib/YAMLLoader.js
@@ -0,0 +1,29 @@
+import { readFileSync, existsSync } from 'fs';
+import { parse } from 'yaml';
+import appRootPath from 'app-root-path';
+
+const { resolve } = appRootPath;
+
+const getFromFile = (file) => {
+  if (existsSync(file)) {
+    const config = parse(readFileSync(file, 'utf8'));
+    return config;
+  }
+  return undefined;
+};
+
+const getAvailableFile = (yamlfile, fallBackFiles = []) => {
+  const processedFiles = [...fallBackFiles, yamlfile].map((file) => resolve(file));
+  const foundedFile = processedFiles.find((file) => existsSync(file));
+  return foundedFile;
+};
+
+export default (yamlFile, options) => {
+  if (options && !options?.fallBack) {
+    throw new Error('fallBack option not found');
+  } else {
+    const foundedFile = getAvailableFile(yamlFile, options?.fallBack || '');
+    if (foundedFile) return getFromFile(foundedFile);
+    throw new Error('No YAML file was founded');
+  }
+};
